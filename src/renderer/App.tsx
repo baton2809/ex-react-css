0a26858f68c1dce714896d42c026a28e7b941996
Artsiom Butomau <artsiom.butomau@mediatech.dev>
2022-05-18 23:06:11 +0300

    add ImageGallery

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,32 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const Hello = () => {
+
+  var images = [
+    {
+      original: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
+      thumbnail: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
+    },
+    {
+      original: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+      thumbnail: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+    },
+  ]
+
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <div className="comp1"/>
+      <ImageGallery
+        thumbnailPosition={"bottom"}
+        useBrowserFullscreen={false}
+        showPlayButton={false}
+        showNav={true}
+        showIndex={true}
+        items={images}
+      />
     </div>
   );
 };
