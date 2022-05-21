79f3f972570d6aaa617cbd504391c0e31ab1687f
Artsiom Butomau <artsiom.butomau@mediatech.dev>
2022-05-21 11:49:42 +0300

    initial project to develop and testing features

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,31 +1,10 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import ImageGallery from 'react-image-gallery';
-import 'react-image-gallery/styles/css/image-gallery.css';
 
 const Hello = () => {
-  let images = [
-    {
-      original: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
-      thumbnail: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
-    },
-    {
-      original: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
-      thumbnail: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
-    },
-  ];
-
   return (
     <div>
-      <div className='comp1' />
-      <ImageGallery
-        thumbnailPosition='bottom'
-        useBrowserFullscreen={false}
-        showPlayButton={false}
-        showNav={true}
-        showIndex={true}
-        items={images}
-      />
+      <h3>Start develop...</h3>
     </div>
   );
 };
@@ -34,7 +13,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Hello />} />
+        <Route path="/" element={<Hello />} />
       </Routes>
     </Router>
   );
